1f773fcfe559360fe95586e9d4519831254e6d81
Дарья <karazanova@bk.ru>
2024-02-21 15:57:07 +0300

    Ex5

---
+++ json_parsing.py
@@ -0,0 +1,5 @@
+import json
+
+text_json = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+obj = json.loads(text_json)
+print(obj['messages'][1]['message'])
