4950e68850a78c66764d62c2b502e817385747a9
Дарья <karazanova@bk.ru>
2024-03-09 21:43:31 +0300

    Ex18

---
+++ tests/test_user_delete.py
@@ -0,0 +1,94 @@
+import requests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+import json
+class TestUserDelete(BaseCase):
+    def test_delete_user_2(self):
+        # Login
+        login_data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response, "auth_sid")
+        token = self.get_header(response, "x-csrf-token")
+        user_id = self.get_json_value(response, "user_id")
+
+        # Delete User(id=2)
+        response1 = requests.delete(f"https://playground.learnqa.ru/api/user/{user_id}",
+                                 headers={"x-csrf-token": token},
+                                 cookies={"auth_sid": auth_sid}
+                                 )
+        Assertions.assert_code_status(response1, 400)
+        assert json.loads(response1.content.decode("utf-8"))[
+                   'error'] == f"Please, do not delete test users with ID 1, 2, 3, 4 or 5.", f'Unexpected content {response.content}'
+
+    def test_user_delete_successful(self):
+        # Register
+        register_data = self.prepare_registration_date()
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=register_data)
+
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+
+        email = register_data['email']
+        password = register_data['password']
+        user_id = self.get_json_value(response, "id")
+
+        # Login
+        login_data = {
+                'email': email,
+                'password': password
+        }
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        # Delete user
+        response2 = requests.delete(f"https://playground.learnqa.ru/api/user/{user_id}",
+                                        headers={"x-csrf-token": token},
+                                        cookies={"auth_sid": auth_sid}
+                                        )
+        Assertions.assert_code_status(response2, 200)
+        assert json.loads(response2.content.decode("utf-8"))['success'] == f"!", f'Unexpected content {response.content}'
+
+        # Get User detailes
+        response3 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id}",
+                                    headers={"x-csrf-token": token},
+                                    cookies={"auth_sid": auth_sid}
+                                    )
+        Assertions.assert_code_status(response3, 404)
+        assert response3.content.decode("utf-8") == f"User not found", f'Unexpected content {response.content}'
+
+    def test_user_delete_auth_another_user(self):
+        # Register
+        register_data = self.prepare_registration_date()
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=register_data)
+
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+
+        user_id = self.get_json_value(response, "id")
+
+        # Login
+        login_data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        # Delete User with Auth another user(id=2)
+        response2 = requests.delete(f"https://playground.learnqa.ru/api/user/{user_id}",
+                                    headers={"x-csrf-token": token},
+                                    cookies={"auth_sid": auth_sid}
+                                    )
+        Assertions.assert_code_status(response2, 400)
+        assert json.loads(response2.content.decode("utf-8"))[
+                   'error'] == f"Please, do not delete test users with ID 1, 2, 3, 4 or 5.", f'Unexpected content {response.content}'
+
+
